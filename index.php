2a64ab712dcf1c8fcdc7702d7c58aee9f7e3ac53
Brandon Dautzenberg <brandondautzenberg@hotmail.com>
2023-11-20 12:10:37 +0100

    Code aangepast zodat Mijn boodschappenlijst in het navigatiemenu geen waarschuwing meer laat zien. Navigeren naar de booschappenlijst via navigatiemenu voegt geen nieuwe artikelen toe.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $loader = new \Twig\Loader\FilesystemLoader("./templates");
 /// $twig = new \Twig\Environment($loader), ["cache" => "./cache/cc"]);
 
 /// VOOR DEVELOPMENT:
-$twig = new \Twig\Environment($loader, ["debug" => true ]);
+$twig = new \Twig\Environment($loader, ["debug" => true]);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 /******************************/
@@ -39,32 +39,33 @@ URL:
 http://localhost/index.php?gerecht_id=4&action=detail
 */
 
+$zoekterm = isset($_GET["zoekterm"]) ? $_GET["zoekterm"] : "zoeken";
 $gerecht_id = isset($_GET["gerecht_id"]) ? $_GET["gerecht_id"] : "detail";
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 $waardering = 0;
 $totaal = 0;
 
-switch($action) {
+switch ($action) {
 
-        case "homepage": {
+    case "homepage": {
             $data = $gerecht->selecteer_gerecht();
             $template = 'homepage.html.twig';
             $title = "Verrukkulluk!";
             break;
         }
 
-        case "detail": {
+    case "detail": {
             $data = $gerecht->selecteer_gerecht($gerecht_id);
             $template = 'detail.html.twig';
             $title = "Detailpagina";
             break;
         }
 
-        case "waardering": {
+    case "waardering": {
             $aantal = $_GET["aantal"];
             $gerecht_id = $_GET["gerecht_id"];
-            header('Content-Type: application/json; charset=utf-8'); 
-            $data = $gerecht_info->add_waardering($gerecht_id, $aantal);
+            header('Content-Type: application/json; charset=utf-8');
+            //$data = $gerecht_info->add_waardering($gerecht_id, $aantal);
             $json = json_encode($data);
             echo $json;
             $waardering = 1;
@@ -72,7 +73,7 @@ switch($action) {
             break;
         };
 
-        case "favoriet": {
+    case "favoriet": {
             $gerecht_id = $_GET["gerecht_id"];
             $user_id = $_GET["user_id"];
             header('Content-Type: application/json; charset=utf-8');
@@ -87,20 +88,22 @@ switch($action) {
             break;
         };
 
-        case "boodschappen": {
+    case "boodschappen": {
             $user_id = $_GET["user_id"];
-            $gerecht_id = $_GET["gerecht_id"];
-            //boodschappen->boodschappen_toevoegen($gerecht_id, $user_id);
+            if (isset($_GET["gerecht_id"])) {
+                $gerecht_id = $_GET["gerecht_id"];
+                $boodschappen->boodschappen_toevoegen($gerecht_id, $user_id);
+                };
             $data = $boodschappen->ophalen_boodschappen($user_id);
             $totaal = $boodschappen->bereken_totaal($user_id);
             $template = 'boodschappen.html.twig';
             $title = 'Boodschappen';
         }
 
-        case "zoeken": {
-            
+    case "zoeken": {
         }
-    };
+};
+
 
 /// Onderstaande code schrijf je idealiter in een layout klasse of iets dergelijks
 /// Juiste template laden, in dit geval "homepage"
